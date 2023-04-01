eca6035275ccb9fec3a701d8c16a41ceb63e0bc9
Eduard <edwardbs876@gmail.com>
2023-04-01 18:07:53 +0300

    add data initialization script

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -1,18 +1,22 @@
 package guru.springframework.msscbeerservice.bootstrap;
 
-import guru.springframework.msscbeerservice.domain.Beer;
 import guru.springframework.msscbeerservice.repositories.BeerRepository;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
+import java.util.UUID;
 
-@Component
+/**
+ * Created by jt on 2019-05-17.
+ */
+//@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
+    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
+    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
+    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
 
     private final BeerRepository beerRepository;
 
@@ -22,42 +26,7 @@ public class BeerLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        loadBeerObjects();
+        //using data.sql file now
+        //   loadBeerObjects();
     }
-
-    private void loadBeerObjects() {
-
-        if(beerRepository.count() == 0) {
-
-            beerRepository.save(Beer.builder()
-                    .beerName("Baltika")
-                    .beerStyle("9")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_1_UPC)
-                    .price(BigDecimal.valueOf(69.96))
-                    .build());
-
-            beerRepository.save(Beer.builder()
-                    .beerName("RADLER")
-                    .beerStyle("LAMAIE")
-                    .quantityToBrew(500)
-                    .minOnHand(12)
-                    .upc(BEER_3_UPC)
-                    .price(BigDecimal.valueOf(13.37))
-                    .build());
-
-            beerRepository.save(Beer.builder()
-                    .beerName("Kozel")
-                    .beerStyle("Bruna")
-                    .quantityToBrew(500)
-                    .minOnHand(12)
-                    .upc(BEER_2_UPC)
-                    .price(BigDecimal.valueOf(13.37))
-                    .build());
-        }
-
-    }
-
-
 }
